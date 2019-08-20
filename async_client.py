16f2740ae13e56dda76ebbbbec5b7ac6a8e30c5d
MaxOstermann <borecnick@mail.ru>
2019-08-20 17:00:18 +0300

    Пример ответа

--- async_client.py
+++ async_client.py
@@ -10,12 +10,33 @@ async def fetch(session, url):
     }
     async with session.post(url, data=json.dumps(data)) as response:
         return await response.text()
+    """
+    Пример ответа
+    {
+    "id": 6,
+    "success": true,
+    "status": "TRIAL",
+    "tariff": {
+        "id": 1,
+        "name": "One website (100MB)",
+        "size": 100,
+        "websites": 1,
+        "databases": 10
+    },
+    "client": {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz"
+    }
+    }
+    """
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://127.0.0.1:8080')
-        print(html)
+        data = await fetch(session, 'http://127.0.0.1:8080')
+        print(data)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
